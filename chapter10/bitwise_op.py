053b9e309d2d05142040bd048a9fe75e266e0fdd
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-21 22:29:57 +0530

    added chapter10

---
+++ chapter10/bitwise_op.py
@@ -0,0 +1,32 @@
+#Bitwise NOT
+a=~2
+
+print(a)
+
+b=~9
+print(b)
+
+# Bitwise XOR (Exclusive OR)
+a=bin(60 ^ 30)
+print(a)
+
+# Bitwise AND
+a=bin(60 & 30)
+print(a)
+
+#Bitwise OR
+a=60 | 30 
+print(a)
+
+#Bitwise Left Shift
+a=2 << 2 
+print(a)
+
+# Bitwise Right Shift
+a=8 >> 2 
+print(a)
+
+# Inplace Operations
+a = 0b001
+a &= 0b010 
+print(a)

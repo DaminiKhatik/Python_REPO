c36d8110fa78ca4c51671b4218a86f5dfa767ecf
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-21 16:47:54 +0530

    added chapter3

---
+++ chapter3/Indentation.py
@@ -0,0 +1,19 @@
+#Simple example
+if 5 > 2:
+  print("Five is greater than two!")
+  
+  # How Indentation is Parsed
+
+a = 5
+if a > 7:
+    print ("foo")
+else:
+    print("bar")
+print("done")
+
+#everything belonging to a function must be indented
+def someFunction(self, a):
+        if a > 5:
+            return True
+        else:
+            return False

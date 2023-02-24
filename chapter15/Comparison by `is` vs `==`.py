5ad481d70f11b1aea1ce4046e637d994deac7b5d
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-24 08:16:21 -0800

    added chapter15

---
+++ chapter15/Comparison by `is` vs `==`.py
@@ -0,0 +1,6 @@
+a = [1, 2, 3, 4, 5]
+b = a # b references a
+a == b # True
+a is b
+print(a)
+print(b)
---
+++ chapter15/chain of comaprision.py
@@ -0,0 +1,7 @@
+
+# chaining comparison operators
+a, b, c, d, e, f = 0, 5, 12, 0, 15, 15
+exp1 = a <= b < c > d 
+exp2 = a is d > f 
+print(exp1)
+print(exp2)

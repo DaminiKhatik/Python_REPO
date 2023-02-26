345d351b62d640bbfb45c57ec3b59e33544a618b
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-26 10:39:30 +0530

    added chapter44

---
+++ chapter44/module.py
@@ -0,0 +1,18 @@
+# importing module calc.py
+from  module import calc
+
+print(calc.add(10, 10))
+
+
+from  module import calc
+
+print(calc.sub(50, 10))
+
+
+# importing module sqrt.py and factorial.py
+
+from  module import sqrt 
+from  module import factorial 
+
+print(sqrt( 16))
+print(factorial(6))
---
+++ chapter44/package.py
@@ -0,0 +1,21 @@
+from package.dog import woof
+from package.hi import hi
+dog.py
+def woof():  
+    print("WOOF!!!")
+hi.py
+def hi():  
+    print("Hello world!")
+    
+    
+    
+from mypckg.mod1 import hello
+from mypckg.mod2 import sum
+mod1.py
+ 
+def hello():
+    print("damini")
+
+res = sum(1, 2)
+print(res)
+

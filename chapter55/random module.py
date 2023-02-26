a6680cd7dc6d8b5ad21a7ee459d05c369e76f2e0
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-26 22:20:49 +0530

    added chapter55

---
+++ chapter55/random module.py
@@ -0,0 +1,27 @@
+#Creating a random user password
+import random
+from string import punctuation, ascii_letters, digits
+symbols = ascii_letters + digits + punctuation
+secure_random = random.SystemRandom() 
+password = "".join(secure_random.choice(symbols)for i in range(10)) 
+print(password)
+
+
+#Create cryptographically secure random numbers
+from random import SystemRandom
+secure_rand_gen = SystemRandom()
+print(secure_rand_gen.randint(0, 20)) 
+
+
+#Random and sequences: shue, choice and sample
+import random 
+from random import shuffle
+laughs = ["Hi", "Ho", "He"]
+random.shuffle(laughs)  
+print(laughs)
+
+#Creating random integers and ﬂoats: randint, randrange, random, and uniform
+import random 
+from random import randint
+a=random.randrange(1000)
+print(a)

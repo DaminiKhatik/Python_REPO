9e9a649a9b09d8d0cb1c8f7b6411346717a1113c
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-27 21:50:46 +0530

    added chapter74

---
+++ chapter74/print function.py
@@ -0,0 +1,16 @@
+#The Print Function 
+
+#print basic
+print('hello world!') 
+foo = 1 
+bar = 'bar' 
+baz = 3.14
+print(foo)  
+print(bar) 
+print(baz)
+print(str(foo) + " " + bar + " " + str(baz)) 
+
+#Print parameters
+print('apples','bananas', 'cherries', sep=', ') 
+print("paragraph1", end="\n\n"); 
+print("paragraph2")

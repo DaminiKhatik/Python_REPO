3c96a4ff4a54380fc9d5f67060783627e5e84174
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-27 08:52:27 +0530

    added chapter61

---
+++ chapter61/defaultbrowser module.py
@@ -0,0 +1,8 @@
+# Opening a URL with Default Browser
+
+import webbrowser 
+webbrowser.open("http://stackoverflow.com")
+webbrowser.open("http://google.com")
+webbrowser.open_new_tab("http://stackoverflow.com")
+
+#
---
+++ chapter61/diff browser.py
@@ -0,0 +1,4 @@
+import webbrowser 
+ff_path = webbrowser.get("C:\Program Files\Internet Explorer\Explorer.exe") 
+ff = webbrowser.get(ff_path) 
+ff.open("http://stackoverflow.com/")

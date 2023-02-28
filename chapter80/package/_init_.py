e4975fec47b4e90c36e2b712547b8571dceb9b35
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-28 21:37:38 +0530

    added chapter80

---
+++ chapter80/package/_init_.py
@@ -0,0 +1,3 @@
+def hello(number):
+    print("this is my function")
+    return number
---
+++ chapter80/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup("mypackage",
+      version="0.1",description="this is my first package",long_description="this is my first package that i build ",
+      author="damini",packages=["mypackage"],license="myownpackage",
+      install_requires=[])

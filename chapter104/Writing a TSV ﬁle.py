6f1ad7c75cddd65d68268972635c03bed23fa1dc
DaminiKhatik <khatikdamini25@gmail.com>
2023-03-01 22:36:14 +0530

    added chapter104

---
+++ chapter104/Writing a TSV ﬁle.py
@@ -0,0 +1,7 @@
+import csv
+with open('output.tsv', 'wt') as out_file: 
+    tsv_writer = csv.writer(out_file, delimiter='\t') 
+    tsv_writer.writerow(['name', 'field'])    
+    tsv_writer.writerow(['Dijkstra', 'Computer Science'])  
+    tsv_writer.writerow(['Shelah', 'Math']) 
+    tsv_writer.writerow(['Aumann', 'Economic Sciences']) 
---
+++ chapter104/reading and writing csv.py
@@ -0,0 +1,11 @@
+
+#Reading and Writing CSV 
+import pandas as pd
+df=pd.read_csv
+mapReducedata = pd.read_csv(r'C:/Users/MyHP/Desktop/PYTHONbook/chapter104/mapReducedata.csv')
+print(df)
+
+#####writing csv in python
+import pandas as pd
+cities = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
+cities.to_csv('cities.csv')

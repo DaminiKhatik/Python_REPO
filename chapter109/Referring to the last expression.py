96e537c7c6f0857e6cac72948b15dc94e4d400eb
DaminiKhatik <khatikdamini25@gmail.com>
2023-03-02 15:36:40 +0530

    added chapter109

---
+++ chapter109/Referring to the last expression.py
@@ -0,0 +1,7 @@
+#Referring to the last expression
+a=3+4
+print(a)
+b=_+3 
+print(b)
+"""To get the value of the last result from your last expression in the console, use an underscore _.
+"""
---
+++ chapter109/help about an object.py
@@ -0,0 +1,8 @@
+from math import sqrt
+help(sqrt)
+
+"""Help on built-in function sqrt in module math:                                                                                                                                   
+
+sqrt(x, /)
+    Return the square root of x.
+"""

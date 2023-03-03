7bf1b6e3f62807bfe6669e96a345143d12970e95
DaminiKhatik <khatikdamini25@gmail.com>
2023-03-03 12:27:18 +0530

    added chapter114

---
+++ chapter114/Use truth value testing.py
@@ -0,0 +1,6 @@
+mySet = (10, 47, 84, 15)
+if mySet:
+
+	print('It has some elements')
+else:
+	print('It has no elements')
---
+++ chapter114/def main.py
@@ -0,0 +1,7 @@
+
+print ("Always executed")
+
+if __name__ == "__main__":
+	print ("Executed when invoked directly")
+else:
+	print ("Executed when imported")

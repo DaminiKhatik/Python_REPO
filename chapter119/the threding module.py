f7befae985fa0325772f01a5cfcd6e6186ef1fdc
DaminiKhatik <khatikdamini25@gmail.com>
2023-03-03 16:00:12 +0530

    added chapter119

---
+++ chapter119/the threding module.py
@@ -0,0 +1,11 @@
+from __future__ import print_function 
+import threading 
+def counter(count):    
+    while count > 0: 
+        print("Count value", count)     
+        count -= 1   
+        return
+t1 = threading.Thread(target=counter,args=(10,)) 
+t1.start() 
+t2 = threading.Thread(target=counter,args=(20,)) 
+t2.start()

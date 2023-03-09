8dbbb12016e239714eb065ccf19b212e547a244e
DaminiKhatik <khatikdamini25@gmail.com>
2023-03-09 21:13:15 +0530

    added chapter181

--- chapter181/Convert any soundﬁle with python and mpeg.py
+++
@@ -1,6 +0,0 @@
-
-from subprocess import check_call
-ok = check_call(['ffmpeg','-i','C:/Users/MyHP/Desktop/PYTHONbook/chapter181/Download-Free-Airtel-New-Ringtone.wav']) 
-if ok:    
-    with open('output.wav', 'rb') as f:
-        wav_file = f.read()
